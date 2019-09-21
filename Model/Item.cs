c55df09d6310be70790d2b21cbde4d208b26720e
Stephen Gacio <sgacio@mail.usf.edu>
2019-09-21 13:34:06 -0400

    created new app, setting up

---
+++ Model/Item.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace inventory_all_the_things.Models
+{
+  public class Item
+  {
+    public int Id { get; set; }
+
+    public int SKU { get; set; }
+
+    public string Name { get; set; }
+
+    public string ShortDescription { get; set; }
+
+    public int NumberInStock { get; set; }
+
+    public int Price { get; set; }
+
+    public DateTime DateOrdered { get; set; } = DateTime.Now;
+  }
+}
